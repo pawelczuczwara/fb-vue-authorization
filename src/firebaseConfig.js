dcdfb89e92384ce1fa4b4835318952323d38c06a
Pawel Czuczwara <pawel.czuczwara@gmail.com>
2019-08-22 18:54:38 +0200

    Remove

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const config = {
-    apiKey: "AIzaSyBSKMEpvByaeVq0-WErZthaIzdI_9mFagY",
-    authDomain: "vue-firebase-test-c3af2.firebaseapp.com",
-    databaseURL: "https://vue-firebase-test-c3af2.firebaseio.com",
-    projectId: "vue-firebase-test-c3af2",
-    storageBucket: "vue-firebase-test-c3af2.appspot.com",
-    messagingSenderId: "30255577805",
-    appId: "1:30255577805:web:3231a2ddd9c7ffa6"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "3",
+    appId: ""
 };
